fbde51d3277ce9a8882197c1e5ecf8e17f0dada8
mustafayasar28 <mmustafayyasar@gmail.com>
2020-11-27 15:26:36 +0300

    Mustafa Yaşar: Resources added, Credits and HowToPlay scene created.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,3 @@
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -28,6 +27,29 @@ public class MainMenu {
         tableViewScene.getStylesheets().addAll(this.getClass().getResource("CreditsStyle.css").toExternalForm());
 
         window.setScene(tableViewScene);
+
+        Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
+        window.setX((screenBounds.getWidth() - window.getWidth()) / 2);
+        window.setY((screenBounds.getHeight() - window.getHeight()) / 2);
+
+        window.show();
+    }
+
+    @FXML
+    public void goToHowToPlayScreen( ActionEvent event) throws IOException {
+        System.out.println( "mahmut");
+        Parent tableViewParent = FXMLLoader.load(getClass().getResource("HowToPlay.fxml"));
+
+        Scene tableViewScene = new Scene(tableViewParent);
+
+        // Stage information
+        Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+        window.setScene(tableViewScene);
+
+
+        tableViewParent.setId("pane");
+        tableViewScene.getStylesheets().addAll(this.getClass().getResource("CreditsStyle.css").toExternalForm());
+
 
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
         window.setX((screenBounds.getWidth() - window.getWidth()) / 2);
