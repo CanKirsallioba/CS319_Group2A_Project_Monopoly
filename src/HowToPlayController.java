65f4e8169611b9cfaf8c9e601d450c2fcc399bb3
mustafayasar28 <mmustafayyasar@gmail.com>
2020-11-27 15:33:13 +0300

    Mustafa Yaşar: Resources added, Credits and HowToPlay scene created.

--- src/HowToPlayController.java
+++ src/HowToPlayController.java
@@ -19,7 +19,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class HowToPlayController implements Initializable{
+public class HowToPlayController implements Initializable {
 
     @FXML
     public Label howToPlayLabel;
