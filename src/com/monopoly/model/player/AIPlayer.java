165c75000c53483d94f70fe3a710fc3a7d389697
Burak Yigit Uslu <burakyuslu@gmail.com>
2020-12-18 02:29:21 +0300

    Added the part of playTurn method that simulates AI's attempt to bail out of jail when it is in jail.

--- src/com/monopoly/model/player/AIPlayer.java
+++ src/com/monopoly/model/player/AIPlayer.java
@@ -1,6 +1,8 @@
 package com.monopoly.model.player;
 
 import com.monopoly.model.player.strategy.AIStrategy;
+import com.monopoly.model.player.strategy.GameStatistics;
+import com.monopoly.model.tiles.property.TitleDeedCard;
 
 public class AIPlayer extends AbstractPlayer {
     AIStrategy aiStrategy;
@@ -25,6 +27,47 @@ public class AIPlayer extends AbstractPlayer {
 
     @Override
     public void playTurn(){
+        // if player is in jail, tries to bail out of jail
+        if( isInJail()){
+            if( getBailOutOfJailCards().size() > 0){
+                setGetOutOfJailChoice(BailOutChoice.BAIL_OUT_CARD);
+            }
+            else if( liquidTotalWorth < getPlayerToken().getBoard().getBoardSalary() / 4){
+                setGetOutOfJailChoice(BailOutChoice.DOUBLE_DICE);
+            }
+            else{
+                if( getBalance() >= (getPlayerToken().getBoard().getBoardSalary() / 4) ){
+                    setGetOutOfJailChoice(BailOutChoice.MONEY);
+                }
+                // TODO add upgradedable && downgradeable (isDowngradeable()) to TitleDeedCard
+                else{
+                    for(TitleDeedCard titleDeedCard: getTitleDeeds()){
+                        if( titleDeedCard.getUpgradeLevel() > 1 && titleDeedCard.isDowngradeable() && getBalance() < (getPlayerToken().getBoard().getBoardSalary() / 4)){
+                            titleDeedCard.downgrade();
+                        }
+                    }
+                    if( getBalance() < (getPlayerToken().getBoard().getBoardSalary() / 4)){
+                        for(TitleDeedCard titleDeedCard: getTitleDeeds()){
+                            if( titleDeedCard.isMortgaged() == false && getBalance() < (getPlayerToken().getBoard().getBoardSalary() /4)){
+                                titleDeedCard.mortgage();
+                            }
+                        }
+                    }
+                    if( getBalance() < (getPlayerToken().getBoard().getBoardSalary() / 4)){
+                        System.out.println( "ERROR: Calculations incorrect!");
+                        setGetOutOfJailChoice(BailOutChoice.DOUBLE_DICE);
+                    }
+                    else{
+                        setGetOutOfJailChoice(BailOutChoice.MONEY);
+                    }
+                }
+            }
+        }
+        // if player is not in jail, plays turn normally
+        else{
+            rollDice();
+        }
+
 
         // roll dice
         // maybe jail
