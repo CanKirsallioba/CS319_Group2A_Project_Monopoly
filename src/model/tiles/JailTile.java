ffd3836d3a58ba1923b3edb90701a065836abd97
cankirimca <cankirimca@gmail.com>
2020-12-19 00:34:38 +0300

    Updated the contents of the hook method in JailTile class.

--- src/model/tiles/JailTile.java
+++ src/model/tiles/JailTile.java
@@ -10,16 +10,24 @@ public class JailTile extends Tile {
     @Override
     protected ArrayList<GameAction> hook(Player player, ArrayList<GameAction> actions) {
 
-        if(player.getGetOutOfJailChoice() != BailOutChoice.WAIT) {
-            if (player.getBailOutOfJailCards().size() != 0) {
-                setActive(actions, "Use Bail Out Of Jail Card", true);
+        if(player.getCurrentTile() instanceof JailTile){
+            if(player.getGetOutOfJailChoice() != BailOutChoice.WAIT) {
+                if (player.getBailOutOfJailCards().size() != 0) {
+                    setActive(actions, "Use Bail Out Of Jail Card", true);
+                }
+                if (player.getLiquidTotalWorth() > 0) { // TODO: Fix the bail out price
+                    setActive(actions, "Pay Bail Bond", true);
+                }
+                setActive(actions, "Roll Dice", true);
+            }else {
+                setActive(actions, "Wait", true);
             }
-            if (player.getLiquidTotalWorth() > 0) { // TODO: Fix the bail out price
-                setActive(actions, "Pay Bail Bond", true);
-            }
-            setActive(actions, "Roll Dice", true);
-        }else {
-            setActive(actions, "Wait", true);
+        }
+        else{
+            setActive(actions, "Use Bail Out of Jail Card", false);
+            setActive(actions, "Pay Bail Bond", false);
+            setActive(actions, "Roll Dice", false);
+            setActive(actions, "Wait", false);
         }
 
         return actions;
