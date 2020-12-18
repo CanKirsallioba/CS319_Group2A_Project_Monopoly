2979ee8390edfdf390ab0e34a4a289694d5947a0
cankirimca <cankirimca@gmail.com>
2020-12-18 13:28:58 +0300

    Updated action names in Tile classes and ActionFactory class

--- src/com/monopoly/model/tiles/IncomeTaxTile.java
+++ src/com/monopoly/model/tiles/IncomeTaxTile.java
@@ -9,13 +9,13 @@ public class IncomeTaxTile extends Tile {
     @Override
     protected ArrayList<GameAction> hook(Player player, ArrayList<GameAction> actions){
         if (player.getTaxOption() != null) {
-            setActive(actions, "Tax Option 1", true);
-            setActive(actions, "Tax Option 2", true);
+            setActive(actions, "Pay Fixed Amount", true);
+            setActive(actions, "Pay With Ratio", true);
             setActive(actions, "Pay Tax", false);
         } else {
             setActive(actions, "Pay Tax", true);
-            setActive(actions, "Tax Option 1", false);
-            setActive(actions, "Tax Option 2", false);
+            setActive(actions, "Pay Fixed Amount", false);
+            setActive(actions, "Pay With Ratio", false);
         }
         return actions;
     }
