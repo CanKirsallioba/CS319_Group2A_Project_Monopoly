a918ce62f1991d65138b3fd25a106450035cb6d7
Burak Yigit Uslu <burakyuslu@gmail.com>
2020-12-19 14:42:24 +0300

    Added GameActions.java file and relevant actions.

---
+++ src/model/tiles/GameActions.java
@@ -0,0 +1,14 @@
+package model.tiles;
+
+public class GameActions {
+    public static final String BUY_PROPERTY_ACTION = "Buy Property";
+    public static final String DONT_BUY_PROPERTY_ACTION = "Don't Buy Property";
+    public static final String MORTGAGE_PROPERTY_ACTION = "Mortgage Property";
+    public static final String UPGRADE_PROPERTY_ACTION = "Upgrade Property";
+    public static final String DOWNGRADE_PROPERTY_ACTION = "Downgrade Property";
+    public static final String APPLY_ACTION = "Apply";
+    public static final String ACCEPT_TRADE_OFFER_ACTION = "Accept Trade Offer";
+    public static final String REJECT_TRADE_OFFER_ACTION = "Reject Trade Offer";
+    public static final String BID_ACTION = "Bid";
+    public static final String DONT_BID_ACTION = "Don't Bid";
+}
