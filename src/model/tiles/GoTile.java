83763312869ed98bdc201ae9765e0ab966dce7b4
cankirimca <cankirimca@gmail.com>
2020-12-20 00:31:19 +0300

    Wrote Javadoc comments for TitleDeedCard and some Tile classes

--- src/model/tiles/GoTile.java
+++ src/model/tiles/GoTile.java
@@ -5,6 +5,13 @@ import model.player.Player;
 import java.util.ArrayList;
 
 public class GoTile extends Tile {
+
+    /**
+     * This method does not have any functionality since it is called on go tile
+     * @param player the player who landed on the tile
+     * @param actions the actions associated with the tile
+     * @return
+     */
     @Override
     protected ArrayList<GameAction> hook(Player player, ArrayList<GameAction> actions) {
         return actions;
