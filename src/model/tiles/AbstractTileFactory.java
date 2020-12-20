b26346b334e2b430fb5f515cf1d2d677a4216bb5
CanKirsallioba <cankirsallioba@gmail.com>
2020-12-20 17:04:22 +0300

    Wrote JavaDoc for the AbstractTileFactory class

--- src/model/tiles/AbstractTileFactory.java
+++ src/model/tiles/AbstractTileFactory.java
@@ -1,8 +1,11 @@
 package model.tiles;
-
 import org.json.simple.JSONObject;
 
-
 public abstract class AbstractTileFactory {
+    /**
+     * Abstract getter function for the tiles, it takes a JSON config object as its argument.
+     * @param config is the config object
+     * @return the tile that is returned with the config data
+     */
     public abstract Tile getTile(JSONObject config);
 }
